86ba3ea5f0558d1f035ef1267abf7a140b40929d
SimJoonYeol <1219530@inha.edu>
2022-02-17 16:17:16 +0900

    Finish Multi Robot Navigation

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'robot_path_planning'
 
@@ -7,9 +9,14 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'params'), glob('params/*.yaml')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.model')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*.pgm')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
